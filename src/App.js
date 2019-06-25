beef9e439f6e4923b78f6143f0fca7e66136b961
Levi Miller <paullevimiller@gmail.com>
2019-06-24 20:03:02 -0700

    Wrestling with Pokemon API inclusion!

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SearchParams from "./SearchParams";
+
+const App = () => {
+  return <SearchParams />;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
