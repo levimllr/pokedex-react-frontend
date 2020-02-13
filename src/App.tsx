a4b7c7cab6e38b66efa1cf155305536ffda81e69
Michael Dixon <michaeldixon@Michaels-MacBook-Air.local>
2020-02-12 19:27:03 -0800

    Fix test

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import React, { useState, useEffect, FunctionComponent } from 'react';
-import { Switch, Route, withRouter } from 'react-router-dom';
-import { RouteComponentProps } from 'react-router';
-import './App.css';
-import Nav from './Nav';
-import CardGrid from './CardGrid';
-import DetailPage from './DetailPage';
-import pokeload from './pikapokeball.gif'
-import { AllPokemonAPI, MetaPokemon, PokemonAttributes } from './types/index'
+import React, { useState, useEffect, FunctionComponent } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Nav from "./Nav";
+import CardGrid from "./CardGrid";
+import DetailPage from "./DetailPage";
+import pokeload from "./pikapokeball.gif";
+import { AllPokemonAPI, MetaPokemon, PokemonAttributes } from "./types/index";
 
-const App:FunctionComponent<RouteComponentProps> = () => {
-
+const App: FunctionComponent<{}> = () => {
   const [allPokemon, setAllPokemon] = useState<Array<PokemonAttributes>>([]);
   // const [currentPokemon, setCurrentPokemon] = useState<Pokemon | null>(null);
 
   const fetchAllPokemon = () => {
-    fetch('http://localhost:3001/api/v1/pokemon')
+    fetch("http://localhost:3001/api/v1/pokemon")
       .then(resp => resp.json())
       .then(json => formatAllPokemonResponse(json));
   };
@@ -23,27 +21,32 @@ const App:FunctionComponent<RouteComponentProps> = () => {
     fetchAllPokemon();
   }, []);
 
-  const formatAllPokemonResponse = (json:AllPokemonAPI) => {
-    let parsedData = json.data.map((pokemon:MetaPokemon) => pokemon.attributes);
+  const formatAllPokemonResponse = (json: AllPokemonAPI) => {
+    let parsedData = json.data.map(
+      (pokemon: MetaPokemon) => pokemon.attributes
+    );
     setAllPokemon(parsedData);
   };
 
   return (
     <div className="App">
-      <Nav numberOfPokemon={allPokemon.length} />
-      { allPokemon.length > 0 ?
-        <Switch>
-          <Route exact path="/pokemon">
-            <CardGrid  allPokemon={allPokemon} />
-          </Route>
-          <Route path="/pokemon/:id">
-            <DetailPage allPokemon={allPokemon} />
-          </Route>
-        </Switch> :
-        <img src={pokeload} alt="Pikachu Pokeball"></img>
-      }
+      <Router>
+        <Nav numberOfPokemon={allPokemon.length} />
+        {allPokemon.length > 0 ? (
+          <Switch>
+            <Route exact path="/pokemon">
+              <CardGrid allPokemon={allPokemon} />
+            </Route>
+            <Route path="/pokemon/:id">
+              <DetailPage allPokemon={allPokemon} />
+            </Route>
+          </Switch>
+        ) : (
+          <img src={pokeload} alt="Pikachu Pokeball"></img>
+        )}
+      </Router>
     </div>
   );
-}
+};
 
-export default withRouter(App);
+export default App;
