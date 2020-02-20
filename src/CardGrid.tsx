4a22c87e44e50bc4dc1ca62124dd77177cc40df7
Levi Miller <paullevimiller@gmail.com>
2020-02-19 20:19:47 -0800

    Remove superfluous import

--- src/CardGrid.tsx
+++ src/CardGrid.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent, useEffect } from 'react';
+import React, { FunctionComponent } from 'react';
 import Card from './Card';
 import { PokemonAttributes } from './types/index';
 
