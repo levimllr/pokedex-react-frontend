377a563084c366857ea4a97c0d389caaa380641f
Levi Miller <44423236+levimllr@users.noreply.github.com>
2020-03-09 19:53:34 -0700

    Lm/detail view (#19)
    * Add divs for Pokemon detail card component
    
    * Set background color of type headers
    
    * Align detail view-header items
    
    * Calculate and format xp, height, weight.
    
    * Format flavor text
    
    * Format stats
    
    * Format moves
    
    * Patch hp stat pop loading issue
    
    * Fix redundant move samples
    
    * Prettify modified files
    
    * Move helper methods into separate file and comment

--- src/DetailView/DetailView.tsx
+++ src/DetailView/DetailView.tsx
@@ -1,19 +1,123 @@
 import React, { FunctionComponent } from 'react';
-import { FullPokemonAttributes } from '../types/index';
+import { FullPokemonAttributes, Type, Stat, Move } from '../types/index';
+import './DetailView.scss';
+import pokeload from '../assets/pikapokeball.gif';
+import {
+  capitalizeFirstLetter,
+  heightConverter,
+  weightConverter,
+} from '../helpers';
 
 interface DetailViewProps {
   pokemon: FullPokemonAttributes;
 }
 
-const DetailView: FunctionComponent<DetailViewProps> = ({ pokemon }) => (
-  <div className="view">
-    <h1>
-      #{pokemon.pokemon_id} {pokemon.name}
-    </h1>
-    <img alt={pokemon.name} src={pokemon.sprites.front_default} />
-    <h3>Height: {pokemon.height} decimeters</h3>
-    <h3>Weight: {pokemon.weight} hectograms</h3>
-  </div>
-);
+const typeStyleName = (types: Array<Type>) => {
+  const prefix = 'type-' + (types.length === 1 ? 'single' : 'double') + '-';
+  return prefix + types.map(type => type.type.name).join('-');
+};
+
+const typeHeader = (types: Array<Type>) => {
+  return types.map(type => (
+    <div className={`type-header-${type.type.name} view-header`}>
+      {type.type.name}
+    </div>
+  ));
+};
+
+const statFormatter = (stats: Array<Stat>) => {
+  return stats.slice(0, 5).map(stat => (
+    <tr>
+      <td className="align-cell-right">
+        {capitalizeFirstLetter(stat.stat.name)}
+      </td>
+      <td className="align-cell-right">{stat.base_stat}</td>
+    </tr>
+  ));
+};
+
+// sample five random moves from the pokemon's list of moves and
+// format them for display
+const moveFormatter = (moves: Array<Move>) => {
+  let moveSample = [];
+  for (let i = 0; i < 5; i++) {
+    let move = moves.slice(Math.floor(Math.random() * moves.length))[0];
+    moveSample.push(move);
+  }
+  return moveSample.map(move => (
+    <tr>
+      <td className="align-cell-left">
+        {capitalizeFirstLetter(move.move.name)}
+      </td>
+    </tr>
+  ));
+};
+
+const DetailView: FunctionComponent<DetailViewProps> = ({ pokemon }) => {
+  if (pokemon) {
+    return (
+      <div className={`view ${typeStyleName(pokemon.types)}`}>
+        <div className="grid-container equal-items">
+          <div className="grid-left justified-self-left">
+            <h2>
+              #{pokemon.pokemon_id} {pokemon.name}
+            </h2>
+          </div>
+          <div className="grid-right justified-self-right">
+            <div className="view-header">{pokemon.stats[5].base_stat} HP</div>
+            {typeHeader(pokemon.types)}
+          </div>
+        </div>
+        <div>
+          <img
+            alt={pokemon.name}
+            src={pokemon.sprites.front_default}
+            className="detail-sprite"
+          />
+        </div>
+        <div>
+          <div className="view-profile">
+            Base XP: {pokemon.base_experience}.
+          </div>
+          <div className="view-profile">
+            Height: {heightConverter(pokemon.height)}.
+          </div>
+          <div className="view-profile">
+            Weight: {weightConverter(pokemon.weight)}.
+          </div>
+        </div>
+        <div className="view-stats grid-container equal-items">
+          <div className="grid-left justified-self-right">
+            <table>
+              <thead>
+                <tr>
+                  <th className="align-cell-right" colSpan={2}>
+                    Starting Stats
+                  </th>
+                </tr>
+              </thead>
+              {statFormatter(pokemon.stats)}
+            </table>
+          </div>
+          <div className="grid-right justified-self-left">
+            <table>
+              <thead>
+                <tr>
+                  <th className="align-cell-left" colSpan={2}>
+                    Learnable Moves
+                  </th>
+                </tr>
+              </thead>
+              {moveFormatter(pokemon.moves)}
+            </table>
+          </div>
+        </div>
+        <div className="view-flavor">{pokemon.flavor_text}</div>
+      </div>
+    );
+  } else {
+    return <img src={pokeload} alt="Pikachu Pokeball" />;
+  }
+};
 
 export default DetailView;
